bb6147f2b77cbacb9c2e455285056a3842888cfa
Sadik <mediajon@iconshots.com>
2022-03-24 18:48:07 +0300

    Update sistem.py

--- sistem.py
+++ sistem.py
@@ -16,14 +16,14 @@ last_use = 1
 def SendVideo(userid,msgid):
 	res = post(f"{api}sendvideo",
     data={"chat_id":userid,
-      "caption":"<b>Video skachat qiluvchi</b> @SSTaTaBot!\n\n<b>EN</b> : <i>agar video oq kelsa ssilkani boshqattan junating!</i>\n<b>ID</b> : <i>yordam uchun aloqaga chiqing</i>",
+      "caption":"<b>Video Downloaded from</b> @TiktokVideoDownloaderIDBot!\n\n<b>EN</b> : <i>if video blank send url again!</i>\n<b>ID</b> : <i>jika video putih kirim url lagi</i>",
       "parse_mode":"html",
       "reply_to_message_id":msgid,
       "reply_markup":json.dumps(
         {"inline_keyboard":[
           [
-            {"text":"Aloqa ^-^",
-            "url":"https://t.me/@Mediajon"
+            {"text":"Support Me ^-^",
+            "url":"https://www.youtube.com/channel/UCTjTQErgDA79Owo6tnYN0PQ?sub_confirmation=1"
             }
           ]
           ]
@@ -53,26 +53,26 @@ def Bot(update):
     msgid = update['message']['message_id']
     timee = update['message']['date']
     if update['message']['chat']['type'] != "private":
-      SendMsg(userid,"Bu bot faqat shaxsiy rejimda ishlaydi !",msgid)
+      SendMsg(userid,"Bot only work in private chat !",msgid)
       return
     first_name = update['message']['chat']['first_name']
     print(f"{get_time(timee)}-> {userid} - {first_name} -> {pesan}")
     if pesan.startswith('/start'):
-      SendMsg(userid,"<b>TaTa Fayl Skachat qilishga hush kelibsiz !</b>\n\n<b>Botni qanday ishlataman </b>:\n<i>video ssilkasini ushbu botga junatishingiz yetarli </i>!!\n",msgid)
+      SendMsg(userid,"<b>Welcome to Tiktok Video Downlaoder Bot !</b>\n\n<b>How to use this bot </b>:\n<i>just send or paste url video tiktok on this bot </i>!!\n",msgid)
     elif "tiktok.com" in pesan and "https://" in pesan :
       getvid = tiktok_module.Tiktok().musicallydown(url=pesan)
       if getvid == False:
-        SendMsg(userid,"<i>Videoni skachat qilolmadi</i>\n\n<i>Yana harakat qlib kuring</i>",msgid)
+        SendMsg(userid,"<i>Failed to download video</i>\n\n<i>Try again later</i>",msgid)
         return
       elif getvid == "private/removed":
-        SendMsg(userid,"<i>Videoni skachat qilolmadi</i>\n\n<i>Video shaxsiyga berkitilgan ekan</i>",msgid)
+        SendMsg(userid,"<i>Failed to download video</i>\n\n<i>Video was private or removed</i>",msgid)
       elif getvid == "file size is to large":
-        SendMsg(userid,"<i>Videoni skachat qilolmadi</i>\n\n<i>Video hajmi juda ham katta ekan</i>",msgid)
+        SendMsg(userid,"<i>Failed to download video</i>\n\n<i>Video size to large</i>",msgid)
       else:
         SendVideo(userid,msgid)
     elif "/help" in pesan:
-      SendMsg(userid,"Botni qanday ishlatsh :\nvideo ssilkasini ushbu botga junatishingiz yetarli !\n\n/donation - for donation bot\n/status - show status bot",msgid)
+      SendMsg(userid,"How to use this bot :\njust send or paste url tiktok video on this bot !\n\n/donation - for donation bot\n/status - show status bot",msgid)
     elif pesan.startswith("/donation"):
-      SendMsg(userid,"Bizn qullab quvvatlang!",msgid)
+      SendMsg(userid,"Support me on\n\nko-fi (EN): https://ko-fi.com/fowawaztruffle\nsaweria (ID): https://saweria.co/fowawaztruffle\ntrakteerid (ID): https://trakteer.id/fowawaz\nQRIS (EWALLET,BANK): https://s.id/nusantara-qr",msgid)
   except KeyError:
     return
